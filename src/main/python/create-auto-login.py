496947421c68525cd3b26f741814377324c89ef2
Eun Sung Allen Kim <siiilver9@gmail.com>
2021-06-07 10:57:32 +0900

    Create create-auto-login.py
    Code for Creon plus auto login.

---
+++ src/main/python/create-auto-login.py
@@ -0,0 +1,16 @@
+
+from pywinauto import application
+import time
+import os
+
+os.system('taskkill /IM coStarter* /F /T')
+os.system('taskkill /IM CpStart* /F /T')
+os.system('taskkill /IM DibServer* /F /T')
+os.system('wmic process where "name like \'%coStarter%\'" call terminate')
+os.system('wmic process where "name like \'%CpStart%\'" call terminate')
+os.system('wmic process where "name like \'%DibServer%\'" call terminate')
+time.sleep(5)        
+
+app = application.Application()
+app.start('C:\CREON\STARTER\coStarter.exe /prj:cp /id: /pwd: /pwdcert: /autostart')  # id: 에 아이디, pwd에 패스워드, pwdcert 에 공인인증서 비밀번호 입력
+time.sleep(60)
