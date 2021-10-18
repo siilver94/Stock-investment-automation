404ee6e354c7a7373c21295ce85cd7318f7fe6ce
Eun Sung Allen Kim <siiilver9@gmail.com>
2021-10-18 22:26:20 +0900

    Update get-stock-item-information.py
    Edit code

--- src/main/python/get-stock-item-information.py
+++ src/main/python/get-stock-item-information.py
@@ -12,6 +12,7 @@ objCpCodeMgr = win32com.client.Dispatch("CpUtil.CpCodeMgr")
 codeList = objCpCodeMgr.GetStockListByMarket(1) #거래소
 codeList2 = objCpCodeMgr.GetStockListByMarket(2) #코스닥
  
+
 print("거래소 종목코드", len(codeList))
 for i, code in enumerate(codeList):
     secondCode = objCpCodeMgr.GetStockSectionKind(code)
